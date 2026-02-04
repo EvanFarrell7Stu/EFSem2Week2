7ad94fca7573c14c03f8801b8479ec9a622b134e
Evan Farrell <g00326940@atu.ie>
2026-02-04 14:26:20 +0000

    Created project

---
+++ src/main/java/ie/atu/efsem2week2/EfSem2Week2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.efsem2week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfSem2Week2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfSem2Week2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/efsem2week2/EfSem2Week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.efsem2week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfSem2Week2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
